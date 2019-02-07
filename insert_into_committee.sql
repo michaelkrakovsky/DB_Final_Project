18dee36c7201dda7923692a61957b6e63ca36457
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2019-02-06 19:26:53 -0500

    First Insert Into Statements

---
+++ insert_into_committee.sql
@@ -0,0 +1,10 @@
+Insert Into `committee members` values ("Michael", "Krakovsky", 1);
+Insert Into `committee members` values ("Andrew", "Greb", 2);
+Insert Into `committee members` values ("Matthew", "Nat", 3);
+Insert Into `committee members` values ("John", "Krack", 4);
+Insert Into `committee members` values ("Chris", "Wallace", 5);
+Insert Into `committee members` values ("Two", "Pac", 6);
+Insert Into `committee members` values ("Tom", "Brady", 7);
+Insert Into `committee members` values ("Michael", "Jordan", 8);
+Insert Into `committee members` values ("Michael", "Shue", 9);
+Insert Into `committee members` values ("The nameless", "The nameless", 10);
