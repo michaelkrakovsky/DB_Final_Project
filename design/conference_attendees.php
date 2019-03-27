447b4d72a4c471b1afa0b0e9c6ebaf37505f9083
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2019-03-27 19:12:16 -0400

    Update conference_attendees.php

--- design/conference_attendees.php
+++ design/conference_attendees.php
@@ -41,7 +41,7 @@
                             '');
 
             $students = getDesiredNames("Student_Session_Schedule", "Students", "AttendeeID", "StudentID", $dbh); 
-            $sponsors = getDesiredNames("Sponsor_Session_Schedule", "Sponsore_Attendee", "SponsorID", "SponsorID", $dbh);
+            $sponsors = getDesiredNames("Sponsor_Session_Schedule", "Sponsor_Attendee", "SponsorID", "SponsorID", $dbh);
             $professi = getDesiredNames("Professional_Session_Schedule", "Professionals", "ProfessionalID", "ProfessionalID", $dbh);
             if ($students == Null) {
                 echo "<p>HOUUUUUUSTON, WE...</p>";
