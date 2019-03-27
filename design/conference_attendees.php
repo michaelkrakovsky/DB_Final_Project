ad580df91b6c01f922309324fe5097d4caa853f2
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2019-03-27 19:13:28 -0400

    Final Touches

--- design/conference_attendees.php
+++ design/conference_attendees.php
@@ -19,7 +19,6 @@
                                             (select Distinct $sessAttendID from $sessAttend) as A
                                             inner join $tbTwo on
                                             A.$sessAttendID=$tbTwo.$tbColTwo");
-                echo "select FirstName, LastName from (select Distinct $sessAttendID from '$sessAttend') as A inner join '$tbTwo' on A.'$sessAttendID'='$tbTwo'.'$tbColTwo'";
                 return $listNames;
             }
 
@@ -43,9 +42,6 @@
             $students = getDesiredNames("Student_Session_Schedule", "Students", "AttendeeID", "StudentID", $dbh); 
             $sponsors = getDesiredNames("Sponsor_Session_Schedule", "Sponsor_Attendee", "SponsorID", "SponsorID", $dbh);
             $professi = getDesiredNames("Professional_Session_Schedule", "Professionals", "ProfessionalID", "ProfessionalID", $dbh);
-            if ($students == Null) {
-                echo "<p>HOUUUUUUSTON, WE...</p>";
-            }
             displaySubCom($students, "Students");
             displaySubCom($sponsors, "Sponsors");
             displaySubCom($professi, "Professional");
