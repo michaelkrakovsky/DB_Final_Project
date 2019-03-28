8bb8edfc22f32476d0ed50ece2f3ebb81c216fee
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2019-03-28 18:03:56 -0400

    Update students.php

--- design/students.php
+++ design/students.php
@@ -59,88 +59,86 @@
           ?>
         </form>
     </div>
-    <div>
-      <h3>Insert A New Student</h3>
-      <?php
+    <h3>Insert A New Student</h3>
+    <?php
 
-          # Function Description: Provide the html to Insert a new Student.
-          # Parameters: None # Returns: None # Throws: None
+        # Function Description: Provide the html to Insert a new Student.
+        # Parameters: None # Returns: None # Throws: None
 
-          function insertNewStudTags() {
-              echo "<h2>Insert A New Student</h2>";
-              echo "<form method='post'>";
-              echo "<h5>Input First Name</h5>";
-              echo "<input type='text' name='firstName' value='First Name'><br>";
-              echo "<h5>Input Last Name</h5>";
-              echo "<input type='text' name='lastName' value='Last Name'><br>";
-              echo "<input type='submit' name='insertStud' value='Insert Attendee'>";
-              echo "</form>";
-          }
+        function insertNewStudTags() {
+            echo "<h2>Insert A New Student</h2>";
+            echo "<form method='post'>";
+            echo "<h5>Input First Name</h5>";
+            echo "<input type='text' name='firstName' value='First Name'><br>";
+            echo "<h5>Input Last Name</h5>";
+            echo "<input type='text' name='lastName' value='Last Name'><br>";
+            echo "<input type='submit' name='insertStud' value='Insert Attendee'>";
+            echo "</form>";
+        }
 
-          # Function Description: Return a valid new student id. (The highest ID + 7)
-          # Parameters: pdo (The database connection) # Throws: None 
-          # Returns: newID (The new ID to insert into the proffesional table)
+        # Function Description: Return a valid new student id. (The highest ID + 7)
+        # Parameters: pdo (The database connection) # Throws: None 
+        # Returns: newID (The new ID to insert into the proffesional table)
 
-          function getNewID($pdo) {
-              $getMaxInt = $pdo->query("Select max(StudentID) from Students");
-              foreach($getMaxInt as $i) {                 # Extract the ID from the PDO
-                  $newid = $i[0];
-              }
-              $i[0] = $i[0] + 7;                  
-              return $i[0];
-          }
-
-          # Function Description: Provide the new hotel room that the student will be directed towards.
-          # Parameters: pdo (The database connection)
-          # Returns: newRoom (The hotel room) # Throws: None
-
-          function getHotelRoom($pdo) {
-            $roomInformation = $pdo->query("select * from 
-                                            (select count(Students.HotelRoom) as NumStudents, HotelRoom.RoomNumber from
-                                            Students right join HotelRoom on HotelRoom.RoomNumber=Students.HotelRoom
-                                            group by HotelRoom.RoomNumber) as A
-                                            where A.NumStudents < 4
-                                            Order By A.NumStudents Desc
-                                            Limit 1");            # Query that gets the hotel room to input the student
-            $newRoom = Null;                # If no rooms are available, a null will be returned.
-            foreach($roomInformation as $r) {
-              $newRoom = $r[0];
+        function getNewID($pdo) {
+            $getMaxInt = $pdo->query("Select max(StudentID) from Students");
+            foreach($getMaxInt as $i) {                 # Extract the ID from the PDO
+                $newid = $i[0];
             }
-            return $newRoom;
+            $i[0] = $i[0] + 7;                  
+            return $i[0];
+        }
+
+        # Function Description: Provide the new hotel room that the student will be directed towards.
+        # Parameters: pdo (The database connection)
+        # Returns: newRoom (The hotel room) # Throws: None
+
+        function getHotelRoom($pdo) {
+          $roomInformation = $pdo->query("select * from 
+                                          (select count(Students.HotelRoom) as NumStudents, HotelRoom.RoomNumber from
+                                          Students right join HotelRoom on HotelRoom.RoomNumber=Students.HotelRoom
+                                          group by HotelRoom.RoomNumber) as A
+                                          where A.NumStudents < 4
+                                          Order By A.NumStudents Desc
+                                          Limit 1");            # Query that gets the hotel room to input the student
+          $newRoom = Null;                # If no rooms are available, a null will be returned.
+          foreach($roomInformation as $r) {
+            $newRoom = $r[0];
           }
+          return $newRoom;
+        }
 
-          # Function Desciption: Insert a new student.
-          # Parameters: fName (The new student first name), lName (The new student last name), 
-          # defaultSession (The default session to insert the student. Consider this the intro session.), 
-          # pdo (The database connection)
-          # Throws: None # Returns: None
+        # Function Desciption: Insert a new student.
+        # Parameters: fName (The new student first name), lName (The new student last name), 
+        # defaultSession (The default session to insert the student. Consider this the intro session.), 
+        # pdo (The database connection)
+        # Throws: None # Returns: None
 
-          function insertNewStudent($fName, $lName, $defaultSession, $pdo) {
-              $newID = getNewID($pdo);
-              $newHotelRoom = getHotelRoom($pdo);
-              if ($newHotelRoom) {
-                $pdo->query("INSERT INTO Students Values ($newID, '$fName', '$lName', '$newHotelRoom')");
-                if (!$pdo) {
-                    echo "The Query Was Invalid\n";
-                } else {
-                    $pdo->query("INSERT INTO Student_Session_Schedule Values ($newID, $defaultSession)");
-                    echo "<p>The Student: ",$fName," ",$lName," is now in Room: ",$newHotelRoom,"</p>";      # Confirmation statement
-                }
+        function insertNewStudent($fName, $lName, $defaultSession, $pdo) {
+            $newID = getNewID($pdo);
+            $newHotelRoom = getHotelRoom($pdo);
+            if ($newHotelRoom) {
+              $pdo->query("INSERT INTO Students Values ($newID, '$fName', '$lName', '$newHotelRoom')");
+              if (!$pdo) {
+                  echo "The Query Was Invalid\n";
               } else {
-                echo "<p>There are no rooms available. Book Sooner Next Time!</p>";
+                  $pdo->query("INSERT INTO Student_Session_Schedule Values ($newID, $defaultSession)");
+                  echo "<p>The Student: ",$fName," ",$lName," is now in Room: ",$newHotelRoom,"</p>";      # Confirmation statement
               }
-          }
+            } else {
+              echo "<p>There are no rooms available. Book Sooner Next Time!</p>";
+            }
+        }
 
-          $dbh = new PDO('mysql:host=localhost;dbname=Assn_1_Committee_And_Attendees',
-                  'root',
-                  '');
+        $dbh = new PDO('mysql:host=localhost;dbname=Assn_1_Committee_And_Attendees',
+                'root',
+                '');
 
-          insertNewStudTags();
-          if(isset($_POST['insertStud'])) {
-              getNewID($dbh);
-              insertNewStudent($_POST['firstName'], $_POST['lastName'], 123456, $dbh);
-          }
-          ?>
-    </div>
+        insertNewStudTags();
+        if(isset($_POST['insertStud'])) {
+            getNewID($dbh);
+            insertNewStudent($_POST['firstName'], $_POST['lastName'], 123456, $dbh);
+        }
+        ?>
 </body>
 </html>
