9d97af2112fd02766ec95fc69ca94febdc54b96a
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2019-03-30 18:08:54 -0400

    Update students.php

--- design/students.php
+++ design/students.php
@@ -144,7 +144,11 @@
                       echo "The Query Was Invalid\n";
                   } else {
                       $pdo->query("INSERT INTO Student_Session_Schedule Values ($newID, $defaultSession)");
+                      ob_start();
                       echo "<p>The Student '",$fName," ",$lName,"' is now in Room: ",$newHotelRoom,"</p>";      # Confirmation statement
+                      sleep(1);
+                      ob_flush();
+                      header("Refresh: 0");
                   }
                 } else {
                   echo "<p>There are no rooms available. Book Sooner Next Time!</p>";
