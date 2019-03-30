d64c45d67f7179fa6babd1451c39b01c15f1f9fb
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2019-03-30 17:10:28 -0400

    sdfsdf

--- design/sponsors.php
+++ design/sponsors.php
@@ -117,8 +117,7 @@
                   if(($_POST["sponsorName"] != 'showAllJobs') && ($_POST["sponsorName"] != 'Null')){
                     $deleted_sponsor = $_POST["sponsorName"];
                     deleteCompany($dbh, $_POST["sponsorName"]);
-                    echo '<p2>',$deleted_sponsor,' has been deleted</p2>';
-                    sleep(1);
+                    #echo '<p2>',$deleted_sponsor,' has been deleted</p2>';
                     unset($_POST["sponsorName"]);
                     header("Refresh: 0");
                   }
